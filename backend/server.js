7cbe5371982e3923058c520350121033a4c1982b
faf <faf73215@gmail.com>
2025-05-28 01:39:12 -0700

    comit

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ mongoose.connect(mongoURI, {
 
 mongoose.connection.on('error', (err) => {
     console.error('MongoDB connection error:', err);
-    isMongoConnected = false;
+    isMongoConnected = true;
 });
 
 mongoose.connection.on('connected', () => {
